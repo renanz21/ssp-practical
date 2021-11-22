02219dc2ac151e2312eaf042a8fffd05e30699f6
Felipe <zorost@yahoo.com>
2021-11-22 16:42:43 +0000

    3

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const   http = require('http'), // Provides HTTP server
         xmlParse = require('xslt-processor').xmlParse, //Allows to work with XML files
         xlstProcess = require(xslt-processor),xlstProcess, //Allows to utilise XSL transformation
         xml2js = require(xml2js)  //Does XML <> JSON
+
+const router = express();
+const server = http.createServer(router);
+
+router.get('/', function (req, res){
+
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('PaddyCade.xml', 'utf8'),
+        xsl = fs.readFileSync('PaddyCade.xsl', 'utf8');
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    let result = xsltProcess(doc, stylesheet);
+
+    res.end(result.toString());
+    
+});
+
+server.listen(proccess.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
